3a3d89dc2c29f42037564d06bb3dd1e4228f156a
20010544 <20010544@LZJ>
2021-08-05 15:01:01 +0800

    New Version

--- C206_CaseStudy/src/OnlineMoneyExchange.java
+++ C206_CaseStudy/src/OnlineMoneyExchange.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
-public class C206_CaseStudy {
+public class OnlineMoneyExchange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ArrayList<CurrencyList> currencyList = new ArrayList<CurrencyList>();
+		
+		int option = -1;
+		
 		
 	}
 
